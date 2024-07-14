90064c8a857e83eb80b4722b99bbaeb43eace197
Ramachandran <114416676+ramachandran30@users.noreply.github.com>
2024-07-14 22:52:39 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,18 @@
+import webbrowser
+
+x=webbrowser.open_new_tab('https://sqliteonline.com/syntax/create_table/')
+a=webbrowser.open_new_tab('https://sqliteonline.com/syntax/create_table/')
+b=webbrowser.open_new_tab('https://sqliteonline.com/syntax/create_table/')
+c=webbrowser.open_new_tab('https://sqliteonline.com/syntax/create_table/')
+d=webbrowser.open_new_tab('https://sqliteonline.com/syntax/create_table/')
+e=webbrowser.open_new_tab('https://sqliteonline.com/syntax')
+f=webbrowser.open_new_tab('https://sqliteonline.com/')
+for i in range(10):
+    print(x)
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+    print(f)
+    
